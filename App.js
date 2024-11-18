20821f2ead252005b65cd4c1f2c288b720437411
GilangHyrs <gilanghadyr@gmail.com>
2024-11-18 19:18:22 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const FlexBoxLayout = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>TUGAS DPM PERTEMUAN 2</Text>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    backgroundColor: '#f5f5f5', 
+    padding: 20, 
+  },
+  text: {
+    position: 'absolute', 
+    fontSize: 24, 
+    color: 'gray', 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    top: '40%', 
+    left: '33%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#4eaae1', 
+    borderRadius: 10,
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#3dd077', 
+    borderRadius: 10,
   },
 });
+
+export default FlexBoxLayout;
